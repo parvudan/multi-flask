1b5303bb989c51509b10f75417170651ea815b27
parvudan <37615018+parvudan@users.noreply.github.com>
2022-12-10 20:26:41 +0100

    Update view.py

--- view.py
+++ view.py
@@ -10,6 +10,6 @@ def home():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = 5001
     app.run(debug=True, host='0.0.0.0', port=port)
     print(f"Running on {port}")
